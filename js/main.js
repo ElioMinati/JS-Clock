4336ccb25f1022a5f5c3cc6cc0ced9f6fa034323
Eliott JOUAN <eliott.jouan@epitech.eu>
2022-06-22 15:41:04 +0200

    [ADD] Working timezones

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-let dt = new Date();
+let dt;
 let hours;
 let minutes;
 let seconds;
 let counter = 0;
+let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 const formatUnit = unit => {
     if (unit < 10) {
@@ -13,7 +14,9 @@ const formatUnit = unit => {
 
 const updateTime = () => {
     counter++;
-    dt = new Date();
+    dt = new Date(new Date(new Date()).toLocaleString('en-US', {timeZone: timezone,}));
+    console.log(timezone);
+    console.log(dt);
     hours = formatUnit(dt.getHours());
     minutes = formatUnit(dt.getMinutes());
     seconds = formatUnit(dt.getSeconds());
@@ -23,5 +26,32 @@ const updateTime = () => {
         document.getElementById('time').innerHTML = `${hours}:${minutes}:${seconds}`;
     }
 }
+
+const setupTimezones = () => {
+    let select = document.querySelector('select');
+    const timezones = Intl.supportedValuesOf('timeZone');
+    if (!timezones) {
+        const error = "Your browser doesn't support timezones.";
+        error.disabled = true;
+        select.options.add(new Option(error))
+        return;
+    }
+    let count = 0;
+    timezones.forEach((tz) => {
+        select.options.add(new Option(tz));
+        if (tz === timezone) {
+            select.selectedIndex = count;
+        }
+        count++;
+    })
+    updateTime();
+}
+
+const changeTimezone = () => {
+    timezone = document.getElementById('timezones').value;
+    updateTime();
+}
+
+window.onload = setupTimezones;
 
 setInterval(updateTime, 1000);
