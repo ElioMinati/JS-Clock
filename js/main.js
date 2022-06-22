8402ce115a5a630dd9dab3bb8b5ca17c909cba8c
Eliott JOUAN <eliott.jouan@epitech.eu>
2022-06-22 18:36:02 +0200

    [UPDATE] seperate js files

--- js/main.js
+++ js/main.js
@@ -7,67 +7,6 @@ let month;
 let counter = 0;
 let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-const formatUnit = unit => {
-    if (unit < 10) {
-        return "0" + unit;
-    }
-    return unit;
-}
-
-const getDayName = nb => {
-    switch(nb) {
-        case 1:
-            return 'Monday';
-        case 2:
-            return 'Tuesday';
-        case 3:
-            return 'Wednesday';
-        case 4:
-            return 'Thursday';
-        case 5:
-            return 'Friday';
-        case 6:
-            return 'Saturday';
-        case 7:
-            return 'Sunday';
-        default:
-            break;
-    }
-    return '';
-}
-
-const getMonthName = nb => {
-    switch(nb) {
-        case 0:
-            return 'january';
-        case 1:
-            return 'febuary';
-        case 2:
-            return 'march';
-        case 3:
-            return 'april';
-        case 4:
-            return 'may';
-        case 5:
-            return 'june';
-        case 6:
-            return 'july';
-        case 7:
-            return 'august';
-        case 8:
-            return 'september';
-        case 9:
-            return 'october';
-        case 10:
-            return 'november';
-        case 11:
-            return 'december';
-        default:
-            break;
-    }
-    return '';
-}
-
 const updateTime = () => {
     counter++;
     dt = new Date(new Date(new Date()).toLocaleString('en-US', {timeZone: timezone,}));
